aad305f6edc47c0531db655405b2e7b1c7e73d98
Mohamed <mohamedmhassann@gmail.com>
2019-07-10 23:12:59 +0200

    Repayment DTO and repayment inputs implementation

--- src/main/java/com/lendico/AssignmentloanPlanGenerator/entities/RepaymentDTO.java
+++ src/main/java/com/lendico/AssignmentloanPlanGenerator/entities/RepaymentDTO.java
@@ -1,5 +1,53 @@
 package com.lendico.AssignmentloanPlanGenerator.entities;
 
+import java.util.Date;
 
+/**
+ * @author Mohamed M.Hassan
+ */
 public class RepaymentDTO {
+    private String loanAmount;
+    private String nominalRate;
+    private int duration;
+    private String startDate;
+
+    public RepaymentDTO(String loanAmount, String nominalRate,
+                        int duration, String startDate) {
+        this.loanAmount = loanAmount;
+        this.nominalRate = nominalRate;
+        this.duration = duration;
+        this.startDate = startDate;
+    }
+
+    public String getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(String loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public String getNominalRate() {
+        return nominalRate;
+    }
+
+    public void setNominalRate(String nominalRate) {
+        this.nominalRate = nominalRate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
 }
--- src/main/java/com/lendico/AssignmentloanPlanGenerator/entities/RepaymentInputs.java
+++ src/main/java/com/lendico/AssignmentloanPlanGenerator/entities/RepaymentInputs.java
@@ -1,4 +1,51 @@
 package com.lendico.AssignmentloanPlanGenerator.entities;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 public class RepaymentInputs {
+    private double loanAmount;
+    private double nominalRate;
+    private int duration;
+    private LocalDateTime startDate;
+
+    public RepaymentInputs(RepaymentDTO repaymentDTO) {
+        this.loanAmount = Double.parseDouble(repaymentDTO.getLoanAmount());
+        this.nominalRate = Double.parseDouble(repaymentDTO.getNominalRate());
+        this.duration = repaymentDTO.getDuration();
+        this.startDate = ZonedDateTime.parse(repaymentDTO.getStartDate()).toLocalDateTime();
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public double getNominalRate() {
+        return nominalRate;
+    }
+
+    public void setNominalRate(double nominalRate) {
+        this.nominalRate = nominalRate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
 }
