e4e98953b5e41d12efbbdeb9b4c26e38aa730db3
Mohamed <mohamedmhassann@gmail.com>
2019-07-10 21:50:13 +0200

    Initialize spring boot app

---
+++ src/main/java/com/lendico/AssignmentloanPlanGenerator/AssignmentLoanPlanGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.lendico.AssignmentloanPlanGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentLoanPlanGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentLoanPlanGeneratorApplication.class, args);
+	}
+
+}
