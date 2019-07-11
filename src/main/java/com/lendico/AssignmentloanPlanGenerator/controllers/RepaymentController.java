454e72fbc5fb75fa2965cf7a4e051108ca58fa04
Mohamed <mohamedmhassann@gmail.com>
2019-07-11 15:48:22 +0200

    adding try catch block to handle exception s

--- src/main/java/com/lendico/AssignmentloanPlanGenerator/controllers/RepaymentController.java
+++ src/main/java/com/lendico/AssignmentloanPlanGenerator/controllers/RepaymentController.java
@@ -3,13 +3,18 @@ package com.lendico.AssignmentloanPlanGenerator.controllers;
 import com.lendico.AssignmentloanPlanGenerator.entities.RepaymentDTO;
 import com.lendico.AssignmentloanPlanGenerator.entities.RepaymentDetailsPlan;
 import com.lendico.AssignmentloanPlanGenerator.entities.RepaymentInputs;
+import com.lendico.AssignmentloanPlanGenerator.exceptions.RepaymentPlanGeneratorException;
 import com.lendico.AssignmentloanPlanGenerator.service.RepaymentPlanGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeParseException;
 
 /**
  * @author Mohamed M.Hassann
@@ -21,10 +26,15 @@ public class RepaymentController {
     RepaymentPlanGeneratorService repaymentPlanGeneratorService;
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity generateLoanPlan(@RequestBody RepaymentDTO repaymentDTO){
-        RepaymentInputs repaymentInputs = null;
-        repaymentInputs = new RepaymentInputs(repaymentDTO);
-        RepaymentDetailsPlan repaymentDetailsPlan = repaymentPlanGeneratorService.generateRepaymentPlan(repaymentInputs);
-        return ResponseEntity.ok(repaymentDetailsPlan);
+    public ResponseEntity generateLoanPlan(@RequestBody RepaymentDTO repaymentDTO) {
+        try{
+            RepaymentInputs repaymentInputs = null;
+            repaymentInputs = new RepaymentInputs(repaymentDTO);
+            RepaymentDetailsPlan repaymentDetailsPlan = repaymentPlanGeneratorService.generateRepaymentPlan(repaymentInputs);
+            return ResponseEntity.ok(repaymentDetailsPlan);
+        }catch(NumberFormatException | DateTimeParseException | RepaymentPlanGeneratorException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+
     }
 }
